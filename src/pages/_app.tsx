89309eefba98f81237fc3ea08c43b0ee190d5a9d
Sergej Pakhomov <psv.ekb@gmail.com>
2022-08-12 01:09:46 +0500

    add random pokemon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,6 +32,6 @@ export default withTRPC<AppRouter>({
 	/**
 	 * @link https://trpc.io/docs/ssr
 	 */
-	ssr: false,
+	ssr: true,
 })(MyApp);
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from 'next'
 // import Head from 'next/head'
 // import Image from 'next/image'
 import { trpc } from "@/utils/trpc";
+import { constants } from 'buffer';
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 
 const Home: NextPage = () => {
-	const { data, isLoading } = trpc.useQuery(["hello", { text: "Sergej" }])
 
-	if (isLoading) return <div>is Loading</div>
-	if (data) return <div>{data.greeting}</div>
+	const [first, second] = getOptionsForVote()
 
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center relative">
@@ -17,9 +17,9 @@ const Home: NextPage = () => {
 			<div className="text-2xl text-center p-8">Which Pokémon is Rounder?</div>
 			<div className="p-2"></div>
 			<div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-				<div className="w-16 h-16 bg-red-200"></div>
+				<div className="w-16 h-16 bg-red-800">{first}</div>
 				<div className="p-8">Vs</div>
-				<div className="w-16 h-16 bg-red-200"></div>
+				<div className="w-16 h-16 bg-red-800">{second}</div>
 			</div>
 		</div>
 	)
