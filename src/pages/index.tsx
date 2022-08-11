b551b50c62c27441c0ea1fd6d8e3dbe249b15820
Sergej Pakhomov <psv.ekb@gmail.com>
2022-08-12 02:08:17 +0500

    style buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import React, { useMemo, useState } from 'react';
 import exp from 'constants';
 
+const btn =
+	"inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
 
 const Home: NextPage = () => {
 
@@ -18,6 +21,11 @@ const Home: NextPage = () => {
 
 	if (firstPokemon.isLoading || secondPokemon.isLoading) return null
 
+	const voteForRoundest = (selected: number) => {
+		// todo : fire mutation to update changes
+		updateIds(getOptionsForVote())
+	}
+
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center relative">
 			{/* <Head> */}
@@ -26,15 +34,20 @@ const Home: NextPage = () => {
 			<div className="text-2xl text-center p-8">Which Pokémon is Rounder?</div>
 			<div className="p-2"></div>
 			<div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-				<div className="w-64 h-64  flex flex-col">
+				<div className="w-64 h-64  flex flex-col items-center">
 					<img src={firstPokemon.data?.sprites.front_default} className="w-full" />
-					<div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+					<div className="text-xl text-center capitalize mt-[-2rem]">
+						{firstPokemon.data?.name}
+					</div>
+					<button className={btn} onClick={() => voteForRoundest(first)}>Rounder</button>
 				</div>
 				<div className="p-8">Vs</div>
-				<div className="w-64 h-64  flex flex-col">
+				<div className="w-64 h-64  flex flex-col items-center">
 					<img src={secondPokemon.data?.sprites.front_default} className="w-full" />
-					<div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
-
+					<div className="text-xl text-center capitalize mt-[-2rem]">
+						{secondPokemon.data?.name}
+					</div>
+					<button className={btn} onClick={() => voteForRoundest(first)}>Rounder</button>
 				</div>
 			</div>
 		</div >
