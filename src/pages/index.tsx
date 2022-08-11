19b9d99e9c79fd0e1eae635cadeacbe4a34f570d
Sergej Pakhomov <psv.ekb@gmail.com>
2022-08-12 01:51:55 +0500

    backend with pokemon data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,19 @@ import type { NextPage } from 'next'
 import { trpc } from "@/utils/trpc";
 import { constants } from 'buffer';
 import { getOptionsForVote } from '@/utils/getRandomPokemon';
+import React, { useMemo, useState } from 'react';
+import exp from 'constants';
+
 
 const Home: NextPage = () => {
 
-	const [first, second] = getOptionsForVote()
+	const [ids, updateIds] = useState(() => getOptionsForVote())
+	const [first, second] = ids
+
+	const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }])
+	const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }])
+
+	if (firstPokemon.isLoading || secondPokemon.isLoading) return null
 
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center relative">
@@ -17,11 +26,18 @@ const Home: NextPage = () => {
 			<div className="text-2xl text-center p-8">Which Pokémon is Rounder?</div>
 			<div className="p-2"></div>
 			<div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-				<div className="w-16 h-16 bg-red-800">{first}</div>
+				<div className="w-64 h-64  flex flex-col">
+					<img src={firstPokemon.data?.sprites.front_default} className="w-full" />
+					<div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+				</div>
 				<div className="p-8">Vs</div>
-				<div className="w-16 h-16 bg-red-800">{second}</div>
+				<div className="w-64 h-64  flex flex-col">
+					<img src={secondPokemon.data?.sprites.front_default} className="w-full" />
+					<div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
+
+				</div>
 			</div>
-		</div>
+		</div >
 	)
 }
 
